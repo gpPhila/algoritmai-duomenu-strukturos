3226e39cfb0faf4977cc97498bf67c9dd7e8bcc1
Ksenija Perechrest <s056857@ad.viko.lt>
2025-02-04 13:53:56 +0200

    masyvo neig elementu suma, didz ir maz masyvo elementai

--- 1-praktine-masyvai/main.cpp
+++ 1-praktine-masyvai/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int masyvoDydis;
+    cout << "Iveskite masyvo dydi:" << endl;
+    cin>>masyvoDydis;
+
+    int masyvas1[masyvoDydis];
+    int skaicius;
+    cout<<"Iveskite masyvo duomenis (per tarpa):"<<endl;
+
+    for (int i = 0; i < masyvoDydis; i++) {
+        cin>>skaicius;
+        masyvas1[i] = skaicius;
+    }
+
+    cout<<"Masyvo duomenys: "<<endl;
+    for (int i = 0; i < masyvoDydis; i++) {
+        cout<<masyvas1[i]<<" ";
+    }
+    cout<<endl;
+
+    //------------------1 uzduotis-----------------------------
+    int neigSkaciuSuma = 0;
+    for (int i = 0; i < masyvoDydis; i++) {
+        if (masyvas1[i] < 0) {
+            neigSkaciuSuma += masyvas1[i];
+        }
+    }
+    cout<<"Neigiamu skaiciu suma: "<<neigSkaciuSuma<<endl;
+
+    int didElementas = masyvas1[0];
+    int mazElementas = masyvas1[0];
+    for (int i = 0; i < masyvoDydis; i++) {
+        if (masyvas1[i] > didElementas) {
+            didElementas = masyvas1[i];
+        }
+    }
+    for (int i = 0; i < masyvoDydis; i++) {
+        if (masyvas1[i] < mazElementas) {
+            mazElementas = masyvas1[i];
+        }
+    }
+    cout<<didElementas<<" "<<mazElementas<<endl;
     return 0;
 }
