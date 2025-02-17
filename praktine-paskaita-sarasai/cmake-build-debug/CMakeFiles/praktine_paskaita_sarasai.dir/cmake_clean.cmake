313c6f3e1910bf7181036782606247d7f7ead0c5
Ksenija Perechrest <s056857@ad.viko.lt>
2025-02-17 13:54:43 +0200

    finished code

---
+++ praktine-paskaita-sarasai/cmake-build-debug/CMakeFiles/praktine_paskaita_sarasai.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/praktine_paskaita_sarasai.dir/main.cpp.obj"
+  "CMakeFiles/praktine_paskaita_sarasai.dir/main.cpp.obj.d"
+  "libpraktine_paskaita_sarasai.dll.a"
+  "praktine_paskaita_sarasai.exe"
+  "praktine_paskaita_sarasai.exe.manifest"
+  "praktine_paskaita_sarasai.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/praktine_paskaita_sarasai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- praktine-paskaita-sarasai/main.cpp
+++ praktine-paskaita-sarasai/main.cpp
@@ -31,9 +31,18 @@ int main()
         iterpti_pabaiga(reiksme);
     }
 
-    cout<<"Ivestas sarasas su 5 elementais iterptais i pradzia:"<<endl;
+    cout<<"Ivestas sarasas su 5 elementais iterptais i pabaiga:"<<endl;
     spausdinti_sarasa();
+    cout<<endl;
 
+    cout<<"Pasalinome elementa is pabaigos: "<<pasalinti_elementa_pabaiga()<<endl;
+    cout<<"Atnaujintas sarasas: "<<endl;
+    spausdinti_sarasa();
+    cout<<endl;
+
+    cout<<"Pasalinome elementa is pradzios: "<<pasalinti_elementa_pradzia()<<endl;
+    cout<<"Atnaujintas sarasas: "<<endl;
+    spausdinti_sarasa();
     return 0;
 }
 
@@ -62,4 +71,27 @@ void spausdinti_sarasa() {
     cout<< pr->duom<<" ";
         pr=pr->kitas;
     }
+}
+
+int pasalinti_elementa_pradzia() {
+    sarasas *el=pradzia; //el t.y. a pointer kuris saugoja rodykles 'pradzia' elementa (pradzia irgi yra rodykle)
+                        //dabar tas elementas turi dvi rodykles: el ir pradzia
+    pradzia=pradzia->kitas; //dabar pradzia rodykle rodo i kitas
+                           //pradzia takes over the role of kitas, ir rodo i antra elementa
+    int sal=el->duom; //sukuriam nauja kintamaji kuris saugos pirmo elemento duomenis
+    free (el); //atlaisvinome memory
+    return sal; //grazinome sal kad funkcijos kvietimas matytu pasalinto elemento duomenis
+}
+
+int pasalinti_elementa_pabaiga() {
+    sarasas *el=pradzia;
+    while (el->kitas->kitas != nullptr) {
+        el=el->kitas;
+    }
+    sarasas *temp=el->kitas;
+    el->kitas=NULL;
+    pabaiga=el;
+    int sal=temp->duom;
+    free (temp);
+    return sal;
 }
