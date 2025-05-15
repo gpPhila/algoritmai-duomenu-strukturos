f5681a478a2c712c0d17fa710feee71e678d2f78
gpPhila <ksenija.perechrest@gmail.com>
2025-05-15 17:20:27 +0300

    beveik baigta

--- kursinis_programa/main.cpp
+++ kursinis_programa/main.cpp
@@ -19,6 +19,22 @@ void Bubble_Sort (int array[ ], int length)
     }
 }
 
+void Bubble_Sort_atv (int array[ ], int length)
+{  int i, j, flag = 1;
+    int temp;
+    for(i = 0; (i < length-1 ) && flag; i++)
+    {flag = 0;
+        for (j=0; j < (length -i -1); j++)
+        {if (array [j+1] > array[j])
+        { temp = array [j];// elementų sukeitimas
+            array [j] = array [j+1];
+            array [j+1] = temp;
+            flag = 1;// Ar buvo nors vienas sukeitimas?
+        }
+        }
+    }
+}
+
 void quickSort(int arr[ ], int left, int right) {
     int i = left, j = right;       int tmp;
     int pivot = arr[(left + right) / 2];
@@ -45,8 +61,9 @@ void quickSort(int arr[ ], int left, int right) {
 }
 
 int main() {
-    ofstream failas("data.txt");
 
+    //random skaiciai
+    ofstream failas("data.txt");
     if (!failas) {
         cout << "Failo nera!" << endl;
         return 1;
@@ -59,47 +76,96 @@ int main() {
         failas << number << endl;
     }
     failas.close();
+    cout << "Random duomenys buvo ivesti." << endl;
+
+    //atvirksciai surikiuoti tie pat random skaiciai
+    int masyvasATV[100000];
+    ifstream duomenys("data.txt");
+    for (int i = 0; i < 100000; ++i) {
+        duomenys >> masyvasATV[i];
+    }
+    duomenys.close();
+    Bubble_Sort_atv(masyvasATV, 100000);
+    ofstream isvedimasAtv("atv_surus_data.txt");
+    for (int i = 0; i < 100000; ++i) {
+        isvedimasAtv << masyvasATV[i] << endl;
+    }
+    isvedimasAtv.close();
+    cout << "Atvirksciai surikiuoti duomenys buvo ivesti." << endl;
+
+    //IS anksto Surikiuoti skaiciai
+    int masyvasSur[100000];
+    ifstream data("data.txt");
+    for (int i = 0; i < 100000; ++i) {
+        data >> masyvasSur[i];
+    }
+    data.close();
+    Bubble_Sort(masyvasSur, 100000);
+    ofstream isvedimasSur("surusiuota_data.txt");
+    for (int i = 0; i < 100000; ++i) {
+        isvedimasSur << masyvasSur[i] << endl;
+    }
+    isvedimasSur.close();
+    cout << "Surikiuoti duomenys buvo ivesti." << endl;
 
     int pasirinkimas;
-    while (pasirinkimas !=3) {
-        cout<<"Pasirinkti rikiavima: "<<endl;
-        cout<<"1. Burbulo rikiavimas."<<endl;
-        cout<<"2. Greitasis rikiavimas."<<endl;
-        cout<<"3. Baigti."<<endl;
+    while (pasirinkimas !=2) {
+        cout<<"Burbulo rikiavimas: "<<endl;
+        cout<<"1. Testi."<<endl;
+        cout<<"2. Baigti."<<endl;
 
         cin>>pasirinkimas;
 
         switch (pasirinkimas) {
-            case 1:
-                cout<<"Pasirinkimas: burbulo rikiavimas."<<endl;
+            case 1: {
                 int kiekis;
+                int masyvas[100000];
+                cout<<"Pasirinkimas: Nesurusiuoti duomenys."<<endl;
                 cout << "Kiek skaiciu nuskaityti is failo? ";
                 cin >> kiekis;
 
-                int masyvas[100000];
                 ifstream failas("data.txt");
+                //failas.open("data.txt");
                 for (int i = 0; i < kiekis; ++i) {
                     failas >> masyvas[i];
                 }
                 failas.close();
 
+                //paprastas random duomenu rikiavimas
                 clock_t start = clock();
                 Bubble_Sort(masyvas, kiekis);
                 clock_t end = clock();
-                cout << "Rikiavimo laikas: " << double(end - start) / CLOCKS_PER_SEC << " sek." << endl;
-
-                ofstream isvedimas("surusiuota_data.txt");
+                cout << "Rikiavimo laikas (nesurusiuoti duomenys): " << double(end - start) / CLOCKS_PER_SEC << " sek." << endl;
+                ofstream isvedimas("tarpinis_rezultatas.txt");
                 for (int i = 0; i < kiekis; ++i) {
                     isvedimas << masyvas[i] << endl;
                 }
                 isvedimas.close();
 
-            break;
+                //atvirksciai surikiuotu duomenu rikiavimas
 
-            case 3: cout<<"Baigta."<<endl;
-            break;
+                Bubble_Sort_atv(masyvas, kiekis);
+                ofstream isvedimasAtv("atv_surus_data.txt");
+                for (int i = 0; i < kiekis; ++i) {
+                    isvedimasAtv << masyvas[i] << endl;
+                }
+                isvedimasAtv.close();
 
-            default: cout<<"Tokio pasirinkimo nera."<<endl;
+                clock_t start2 = clock();
+                Bubble_Sort(masyvas, kiekis);
+                clock_t end2 = clock();
+                cout << "Rikiavimo laikas (nesurusiuoti duomenys): " << double(end - start) / CLOCKS_PER_SEC << " sek." << endl;
+
+                break;
+            }
+            case 2: {
+                cout<<"Baigta."<<endl;
+
+                break;
+            }
+            default: {
+                cout<<"Tokio pasirinkimo nera."<<endl;
+            }
         }
     }
     return 0;
